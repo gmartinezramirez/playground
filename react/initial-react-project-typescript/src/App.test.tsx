c6c17b9a8386c506c0f93bbd817bb62a25aa8bbc
gmartinezramirez <gonzalo.martinez.ra@gmail.com>
2021-04-25 12:21:05 -0400

    feat(react skeleton typescript): add prettier config, jest-when and ts rules

--- react/initial-react-project-typescript/src/App.test.tsx
+++ react/initial-react-project-typescript/src/App.test.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Given render App component When get by text learn react Then expect text to be in document", () => {
+  // Given
   render(<App />);
+  // When
   const linkElement = screen.getByText(/learn react/i);
+  // Then
   expect(linkElement).toBeInTheDocument();
 });
